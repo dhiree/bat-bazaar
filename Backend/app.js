f594b6792f0bc78461423194e5602dcb42b5c47d
dheeri1 <dheerendra.singh@evervent.in>
2025-03-17 12:07:36 +0530

    create schema

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import { Server } from 'socket.io';
+import {connectDb} from "./src/config/connectDb"
+import userRouter from "./src/routes/userRoute"
 
 const app = express();
 const port = 3000;
+
+connectDb()
 
 // Initialize Socket.IO directly with the Express app
 const server =app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+app.use("/user",userRouter)
 const io = new Server(server);
-console.log("🚀 ~ io:", io)
 
 io.on('connection', (socket) => {
     console.log('Socket Connection', socket.id);
---
+++ Backend/src/routes/userRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const router = express.Router()
+
+router.post("/")
+router.get("/")
+router.get("/")
+router.put("/")
+router.delete("/")
+
+
+export default router
